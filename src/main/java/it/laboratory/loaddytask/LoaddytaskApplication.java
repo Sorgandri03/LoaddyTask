50c3921670818ddecad768f86001bd72fd750505
MichaelAldeni <michael.aldeni@icloud.com>
2025-04-07 14:56:12 +0200

    setup init

---
+++ src/main/java/it/laboratory/loaddytask/LoaddytaskApplication.java
@@ -0,0 +1,13 @@
+package it.laboratory.loaddytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaddytaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoaddytaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/laboratory/loaddytask/LoaddytaskApplicationTests.java
@@ -0,0 +1,13 @@
+package it.laboratory.loaddytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaddytaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
