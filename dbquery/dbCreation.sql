38ed9128d21ccedd34ea71b3bf5a7acfd741c5c0
Lorenzo Berretta <lberretta4@gmail.com>
2025-07-05 19:40:15 +0200

    added query for creating and filling the database

---
+++ dbquery/dbCreation.sql
@@ -0,0 +1,81 @@
+drop table if exists team cascade ;
+drop table if exists have cascade;
+drop table if exists require cascade;
+drop table if exists task cascade;
+drop table if exists employer cascade;
+drop table if exists skills cascade;
+drop table if exists employee cascade;
+drop table if exists partOf cascade;
+
+
+CREATE TABLE IF NOT EXISTS employee
+(
+    email      VARCHAR(50) PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL,
+    surname    VARCHAR(50) NOT NULL,
+    birthDate  DATE,
+    cellphone  VARCHAR(15),
+    password   VARCHAR(50) NOT NULL
+
+
+);
+
+CREATE TABLE IF NOT EXISTS employer
+(
+    email      VARCHAR(50) PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL,
+    surname    VARCHAR(50) NOT NULL,
+    birthDate  DATE,
+    cellphone  VARCHAR(15) NOT NULL,
+    password   VARCHAR(50) NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS skills
+(
+    idSkills    serial PRIMARY KEY,
+    name        VARCHAR(254) not null,
+    description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS task
+(
+    idTask      serial PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    description TEXT,
+    startDate   DATE,
+    endDate     DATE,
+    status      VARCHAR(50),
+    emailEmployee  VARCHAR REFERENCES employee (email)
+);
+
+CREATE TABLE IF NOT EXISTS require
+(
+    idSkills INT REFERENCES skills (idSkills) on delete cascade on update cascade,
+    idTask   INT REFERENCES task (idTask) on delete cascade on update cascade,
+    PRIMARY KEY (idSkills, idTask)
+);
+
+CREATE TABLE IF NOT EXISTS have
+(
+    emailEmployee VARCHAR REFERENCES employee (email) on delete cascade on update cascade,
+    idSkills   serial REFERENCES skills (idSkills) on delete cascade on update cascade,
+    PRIMARY KEY (emailEmployee, idSkills)
+);
+
+CREATE TABLE IF NOT EXISTS team
+(
+    idTeam      serial PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    description TEXT,
+    emailEmployer  VARCHAR NOT NULL REFERENCES employer (email) on delete cascade on update cascade
+
+);
+
+CREATE TABLE IF NOT EXISTS partOf
+(
+    idTeam     INT REFERENCES team (idTeam) on delete cascade on update cascade,
+    emailEmployee VARCHAR REFERENCES employee (email) on delete cascade on update cascade,
+    PRIMARY KEY (idTeam, emailEmployee)
+);
+
