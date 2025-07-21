804ee1a7d5fee419887bfdff473848ab2bad8e48
Lorenzo <lberretta4@gmail.com>
2025-07-21 15:46:57 +0200

    dockerization of the custom load balance algorithm

---
+++ microservices/algorithm/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import microservices.algorithm.customLoadBalance as customLoadBalance
+app = Flask(__name__)
+
+@app.route('/algorithm', methods=['GET'])
+def customLoadBalance():
+    return "Algorithm service is running!"
+    # return customLoadBalance.customloadbalance(ListOfEmployees, tasksList)
+
+@app.route('/')
+def index():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Microservice hub</title>
+    </head>
+    <body>
+        <h1>Welcome to the Task Balancer App</h1>
+        <p><a href="/algorithm">Go to Algorithm Page</a></p>
+    </body>
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
