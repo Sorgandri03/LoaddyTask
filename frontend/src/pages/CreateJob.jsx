50e5a9b4e331c148b501d80302889ebe2bdde8f7
Sorgandri03 <sorgandri03@gmail.com>
2025-07-22 13:42:26 +0200

    Added Frontend Create Job

--- frontend/src/pages/CreateJob.jsx
+++ frontend/src/pages/CreateJob.jsx
@@ -12,7 +12,7 @@ import {
     TableRow,
     TableCell,
     TextField,
-    OutlinedInput, MenuItem, ListItemText, Select, InputLabel, FormControl
+    OutlinedInput, MenuItem, ListItemText, Select, InputLabel, FormControl, Grid
 } from "@mui/material";
 import Footer from "../components/Footer";
 import {createTheme} from "@mui/material/styles";
@@ -58,32 +58,84 @@ function Skills(member) {
     );
 }
 
+function createTask(name, desc, endDate, weight, selected, prevTasks, setTask) {
+    let task = [];
+    task.push(name);
+    task.push(desc);
+    task.push(endDate);
+    task.push(weight);
+    task.push(selected);
+    setTask(prevTasks => [...prevTasks, task]);
+}
+
+function createJob(tasks) {
+    console.log(tasks);
+}
+
 function AddTask() {
-    const [task, setTask] = React.useState(null);
+    const [tasks, setTask] = React.useState([]);
+    const [name, setName] = React.useState("");
+    const [desc, setDesc] = React.useState("");
+    const oneWeekFromNow = new Date();
+    oneWeekFromNow.setHours(oneWeekFromNow.getHours() + 168);
+    const [endDate, setEndDate] = React.useState(oneWeekFromNow.toISOString().split('T')[0]);
+    const [selected, setSelected] = React.useState([]);
+    const [weight, setWeight] = React.useState(0);
     const [skills, setSkills] = React.useState([]);
     React.useEffect(() => {
         getSkills().then((data) => {
             setSkills(data);
         });
     }, []);
-    if (!task) {
-        return (
-            <React.Fragment>
-                <Box sx={{ p: 4, display: 'flex', justifyContent: 'center' }}>
-                    <ThemeProvider theme={theme}>
+
+    const handleChange = (event) => {
+        setSelected(event.target.value);
+    };
+
+    const emptyTask = () => {
+        setName("");
+        setDesc("");
+        setEndDate(oneWeekFromNow.toISOString().split('T')[0]);
+        setWeight(0);
+        setSelected([]);
+    }
+
+    return (
+        <React.Fragment>
+            <Box sx={{ p: 4, display: 'flex', justifyContent: 'center' }}>
+                <ThemeProvider theme={theme}>
+                    <Grid container direction="column" alignItems="center">
+                        {tasks.map((task) => (
+                            <React.Fragment>
+                                <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+                                    <TextField disabled type="text" label="Task Name" style={{ width: 150 }} value={task[0]} />
+                                    <TextField disabled type="text" label="Description" style={{ flex: 2 }} value={task[1]} />
+                                    <TextField disabled type="date" label="End Date" style={{ width: 160 }} value={task[2]} />
+                                    <TextField disabled type="number" label="Weight" style={{ width: 120 }} value={task[3]} />
+                                    <TextField disabled type="text" label="Skills" style={{ width: 300 }} value={task[4].join(', ')} />
+                                </Box>
+                                <br />
+                            </React.Fragment>
+                        ))}
+                        { tasks.length > 0 && (
+                            <Button variant="contained" color="primary" sx={{ justifyContent: 'center', mb: 4 }} onClick={() => {createJob(tasks)}}>
+                                Create New Job
+                            </Button>
+                        )}
                         <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
-                            <TextField type="text" placeholder="Task Name" style={{ width: 150 }} />
-                            <TextField type="text" placeholder="Description" style={{ flex: 2 }} />
-                            <TextField type="date" placeholder="End Date" style={{ width: 160 }} />
-                            <TextField type="number" placeholder="Weight" style={{ width: 120 }} />
-                            <FormControl sx={{ m: 1, width: 300 }}>
-                                <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+                            <TextField type="text" label="Task Name" style={{ width: 150 }} value={name} onChange={(e)=> setName(e.target.value)} />
+                            <TextField type="text" label="Description" style={{ flex: 2 }} value={desc} onChange={(e)=> setDesc(e.target.value)}/>
+                            <TextField type="date" label="End Date" style={{ width: 160 }} value={endDate} onChange={(e)=> setEndDate(e.target.value)}/>
+                            <TextField type="number" label="Weight" style={{ width: 120 }} slotProps={{ htmlInput: {min: 0, max: 10} }} value={weight} onChange={(e)=> setWeight(e.target.value)}/>
+                            <FormControl sx={{ width: 300 }}>
+                                <InputLabel id="skills-multi-label">Skills</InputLabel>
                                 <Select
-                                    labelId="demo-multiple-checkbox-label"
-                                    id="demo-multiple-checkbox"
+                                    labelId="skills-multi-label"
+                                    id="skills-multi"
                                     multiple
-                                    value={Array.isArray(task?.tags) ? task.tags : []}
-                                    input={<OutlinedInput label="Tag" />}
+                                    value={selected}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="Skills" />}
                                     variant="outlined"
                                     renderValue={(selected) => selected.join(', ')}
                                 >
@@ -94,18 +146,17 @@ function AddTask() {
                                     ))}
                                 </Select>
                             </FormControl>
-                            <TextField type="text" placeholder="Employee (optional)" style={{ flex: 2 }} />
                         </Box>
-                        <br /> <br />
-
-                    </ThemeProvider>
-                </Box>
-                <Button variant="contained" color="primary" sx={{ justifyContent: 'center' }} onClick={() => {}}>
+                    </Grid>
+                </ThemeProvider>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Button variant="contained" color="primary" sx={{ justifyContent: 'center' }} onClick={() => {createTask(name, desc, endDate, weight, selected, tasks, setTask); emptyTask();}}>
                     Add Task
                 </Button>
-            </React.Fragment>
-        );
-    }
+            </Box>
+        </React.Fragment>
+    );
 }
 
 function CreateJob(){
