f4f54c96f2bc67dab279fc81d9a6b4cb52e3a1ef
MichaelAldeni <michael.aldeni@icloud.com>
2025-07-23 17:55:03 +0200

    added data metrics

---
+++ DataMetrics.js
@@ -0,0 +1,16 @@
+[
+    {
+        "set_id": 1,
+        "set_name": "load-balancing-algorithm",
+        "user_stories": 5,
+        "links": [],
+        "db": "true"
+    },
+    {
+        "set_id": 2,
+        "set_name": "<name of microservice>",
+        "user_stories": 8,
+        "links": [],
+        "db": "false"
+    }
+]
